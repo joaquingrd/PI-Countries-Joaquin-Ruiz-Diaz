1864c3e95c8f5a8797098634e3721907897d553a
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-03-10 16:04:53 -0300

    add Access-Control-Allow-Origin

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", URL_PATH_ACC_ORIGIN); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
